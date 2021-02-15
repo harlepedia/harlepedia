ee2c58e73f9b6c18a75be65886b8b3f62626642d
ToddMcAlinden <tdjmcalinden@gmail.com>
2021-02-15 21:41:59 +1300

    Init Structure

--- src/js/main.js
+++ src/js/main.js
@@ -3,7 +3,6 @@
 
 // Imports //
 
-import Swup from "swup";
 import moduleHomePage from "./pages/home-page";
 import modulePage from "./pages/page";
 
@@ -29,23 +28,6 @@ function init(){
 
 // Page Transition Config //
 
-const swup = new Swup({
-
-	cache: true,
-	preload: true,
-	scroll: true,
-	support: true,
-	disableIE: false,
-	elements: ["#swup"],
-	animationSelector: ".page-transition-animation",
-	animateScrollToAnchor: false,
-	animateScrollOnMobile: false,
-	doScrollingRightAway: false,
-	scrollDuration: 0,
-  	LINK_SELECTOR: ".page-transition-target",
-});
-
 init();
 
-swup.on("contentReplaced", init);
 
